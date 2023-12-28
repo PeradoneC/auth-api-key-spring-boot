cb4bacc00bcb3cd2ce3d12891226d35df86cf796
Peradone Chemmookda <peradone.c@gmail.com>
2023-12-28 18:50:49 +0700

    Setup file.

---
+++ src/main/java/com/peradone/authapikeyspringboot/AuthApiKeySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.peradone.authapikeyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiKeySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApiKeySpringBootApplication.class, args);
+	}
+
+}
